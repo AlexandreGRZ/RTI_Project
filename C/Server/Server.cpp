ac26564f6451dc7bf07643659d5b0d3c5df17520
Cyril Russe <cyril.russe@hotmail.com>
2023-09-24 12:03:52 +0200

    init Server dir + clean objets files + add gitignore

---
+++ C/Server/Server.cpp
@@ -0,0 +1,17 @@
+#include "SocketLibrary.h"
+#include <pthread.h>
+#include <mysql.h>
+
+#define MAX_CLIENT 10
+
+typedef struct
+{
+
+} CLIENT;
+
+int main()
+{
+   CLIENT table_client[MAX_CLIENT];
+
+   return 0;
+}
