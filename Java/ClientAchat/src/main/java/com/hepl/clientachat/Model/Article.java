1ad7f5ee464567db6bb999e5329f1eb8fae37698
Hexalthy219 <cyril.russe@hotmail.com>
2023-10-26 23:32:34 +0200

    Interface ClientAchat java basic setup

---
+++ Java/ClientAchat/src/main/java/com/hepl/clientachat/Model/Article.java
@@ -0,0 +1,19 @@
+package com.hepl.clientachat.Model;
+
+public class Article {
+    private int id;
+    private String name;
+    private float price;
+    private int stock;
+    private int quantity;
+
+    public Article(int id, String name, float price, int stock){
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.stock = stock;
+    }
+
+    public Article(String name, )
+
+}
