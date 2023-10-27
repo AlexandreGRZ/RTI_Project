f28de25e70b2725797e2c22e3135272596fd68af
cyril <cyril.russe@student.hepl.be>
2023-10-27 11:35:52 +0200

    Setup button handlers and tableView update

--- Java/ClientAchat/src/main/java/com/hepl/clientachat/Controller.java
+++ Java/ClientAchat/src/main/java/com/hepl/clientachat/Controller.java
@@ -1,14 +1,79 @@
 package com.hepl.clientachat;
 
+import com.hepl.clientachat.Model.Article;
+import com.hepl.clientachat.Model.Model;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.control.*;
+import javafx.scene.image.ImageView;
 
 public class Controller {
     @FXML
-    private Label welcomeText;
-
+    private TextField TextFieldLogin;
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private TextField TextFieldPassword;
+    @FXML
+    private CheckBox checkBoxNewUser;
+    @FXML
+    private Spinner SpinnerQuantity;
+    @FXML
+    private ImageView imageArticle;
+    @FXML
+    private TextField cartPrice;
+    @FXML
+    private TableView TableViewPanier;
+    @FXML
+    private TableColumn<Article, String> ColumnArticle;
+    @FXML
+    private TableColumn<Article, String> ColumnPrice;
+    @FXML
+    private TableColumn<Article, String> ColumnQuantity;
+
+    private Model model;
+
+    public void initialize(){
+        model = Model.getInstance();
+    }
+
+    // Button click handlers--------------------------------------------------------------------------------------------
+    @FXML
+    protected void onLoginClick() {
+
+    }
+    @FXML
+    protected void onLogoutClick() {
+
+    }
+    @FXML
+    protected void onNextClick() {
+
+    }
+    @FXML
+    protected void onPreviousClick() {
+
+    }
+    @FXML
+    protected void onBuyClick() {
+
+    }
+    @FXML
+    protected void onDeleteArticleClick(){
+
+    }
+    @FXML
+    protected void onEmptyCartClick(){
+
+    }
+    @FXML
+    protected void onConfirmClick(){
+
+    }
+
+    // Update cart tableView with article in cart from model
+    private void setCartTable(){
+        TableViewPanier.getItems().setAll(model.getCart());
+
+        ColumnArticle.setCellValueFactory(c -> c.getValue().articleProperty());
+        ColumnPrice.setCellValueFactory(c -> c.getValue().priceProperty());
+        ColumnQuantity.setCellValueFactory(c -> c.getValue().quantityProperty());
     }
 }
